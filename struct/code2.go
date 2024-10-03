02df8655de008e8b42886f7b38fa085fea793107
Sourin <iamsourinsarkar@gmail.com>
2024-10-03 09:28:42 +0530

    Issue: main package shows error when two go files are in same dir

---
+++ struct/code2.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+// initializa the function Rectangle
+type Rectangle func(int, int) int
+
+// create a struct
+type NewRectangle struct {
+	length int
+	breadth int
+	rect Rectangle
+}
+
+func main() {
+	rectInstance := NewRectangle {
+		length: 10,
+		breadth: 20,
+		rect: func(length int, breadth int) int {
+			return length * breadth
+		},
+	}
+
+	fmt.Println(rectInstance.rect(rectInstance.length, rectInstance.breadth))
+}
