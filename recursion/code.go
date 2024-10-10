510ea789a9440d88ad27531102a2ae3b6efc1011
Sourin <iamsourinsarkar@gmail.com>
2024-10-10 17:56:50 +0530

    Done with recursion part

--- recursion/code.go
+++ recursion/code.go
@@ -16,8 +16,20 @@ func facotrial(n int) int {
 	return n * facotrial(n - 1);
 }
 
+func displayNumber() func() int {
+	number := 10
+
+	return func() int {
+		number++
+		return number
+	}
+}
+
 func main() {
 	n := 0
-	fmt.Println("Sum:", sum(n))
+	// fmt.Println("Sum:", sum(n))
 	fmt.Println("Factorial:", facotrial(n))
+	
+	dn := displayNumber()
+	fmt.Println(dn())
 }
